504b81abe3dcbdadee75b4f716e2a0e421a27b21
Matthew Rasmus <mattr@zzntd.com>
2014-12-20 15:07:25 -0800

    Simplified example with multi line input

--- examples/example.rs
+++ examples/example.rs
@@ -5,20 +5,14 @@ use rusql::{rusql_exec, Rusql, TableEntry, ColumnDef};
 fn main() {
     let mut db = Rusql::new();
 
-    rusql_exec(&mut db, "CREATE TABLE Foo(Id INTEGER PRIMARY KEY, Name TEXT);".to_string(), |_,_| {});
+    let sql_str = "CREATE TABLE Foo(Id INTEGER PRIMARY KEY, Name TEXT); \
+                   INSERT INTO Foo VALUES(1, \"Bar1\"); \
+                   INSERT INTO Foo VALUES(2, \"Bar2\"); \
+                   INSERT INTO Foo VALUES(3, \"Bar3\"); \
+                   INSERT INTO Foo VALUES(4, \"Bar4\"); \
+                   SELECT * FROM Foo;";
 
-    let sql_strs = vec![
-        "INSERT INTO Foo VALUES(1, \"Bar1\");",
-        "INSERT INTO Foo VALUES(2, \"Bar2\");",
-        "INSERT INTO Foo VALUES(3, \"Bar3\");",
-        "INSERT INTO Foo VALUES(4, \"Bar4\");",
-    ];
-
-    for sql_str in sql_strs.iter() {
-        rusql_exec(&mut db, sql_str.to_string(), |_,_| {});
-    }
-
-    rusql_exec(&mut db, "SELECT * FROM Foo;".to_string(), |entry: &TableEntry, def: &Vec<ColumnDef>| {
+    rusql_exec(&mut db, sql_str.to_string(), |entry: &TableEntry, def: &Vec<ColumnDef>| {
         for (column, column_def) in entry.iter().zip(def.iter()) {
             println!("{}: {}", column_def.name, column);
         }
