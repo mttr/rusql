78d5c8e2b8a8b2b237c205c2e17b9d5d71d9b531
Matthew Rasmus <mattr@zzntd.com>
2014-12-28 12:30:17 -0800

    Add GNU Readline optional feature for shell
    rename: rusql_cli.rs -> shell.rs

--- src/shell.rs
+++ src/shell.rs
@@ -1,20 +1,28 @@
 extern crate rusql;
+#[cfg(feature = "shellnav")]
+extern crate readline;
 
+#[cfg(feature = "shellnav")]
+use readline::readline;
 use rusql::{rusql_exec, Rusql};
 
+#[cfg(not(feature = "shellnav"))]
 use std::io;
+
+#[cfg(not(feature = "shellnav"))]
+fn readline(prompt: &str) -> Option<String> {
+    print!("{}", prompt);
+
+    io::stdin().read_line().ok()
+}
 
 pub fn main() {
     let mut db = Rusql::new();
     loop {
-        print!("rusql> ");
-
-        let input = io::stdin().read_line()
-                               .ok()
-                               .expect("Failed to read line");
+        let input = readline("rusql> ").expect("Error reading line");
 
         match input.as_slice() {
-            ".make_foo\n" => {
+            ".make_foo" => {
                 rusql_exec(&mut db, "CREATE TABLE Foo(Id INTEGER, Name TEXT);
                                      INSERT INTO Foo VALUES
                                             (1, \"Foo1\"), (2, \"Foo2\"), (3, \"Foo3\");",
